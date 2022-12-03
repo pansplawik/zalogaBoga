cd1706f4b0ccdc5971e38640bd69d0de711180eb
Kamil Spławiński <84287745+pansplawik@users.noreply.github.com>
2022-12-03 18:02:21 +0100

    Update cos.js

--- cos.js
+++ cos.js
@@ -6,9 +6,9 @@ let hour = 00;
 let minute = 00;
 let second = 00;
 let count = 00;
-// const sqlite3 = require('sqlite3');
-// const db = new sqlite3.Database('./db.sqlite');
-// db.run("CREATE TABLE czas (HowLong INT,dateActivity date,typeActivity INT)");
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./db.sqlite');
+db.run("CREATE TABLE czas (HowLong INT,dateActivity date,typeActivity INT)");
 if(startBtn){
 startBtn.addEventListener('click', function () {
 	timer = true;
@@ -64,11 +64,6 @@ function stopWatch() {
 		let minString = minute;
 		let secString = second;
 		let countString = count;
-
-		if (hour < 10) {
-			hrString = "0" + hrString;
-		}
-
 		if (minute < 10) {
 			minString = "0" + minString;
 		}
@@ -80,35 +75,56 @@ function stopWatch() {
 		if (count < 10) {
 			countString = "0" + countString;
 		}
-
+		if(minute==40){
+			timer = false;
+	let hrr = document.getElementById('hr').innerHTML 
+	let minn = document.getElementById('min').innerHTML
+	let ile = minn;
+	const now = new Date();
+	hour = 0;
+	minute = 0;
+	second = 0;
+	count = 0;
+	document.getElementById('hr').innerHTML = "00";
+	document.getElementById('min').innerHTML = "00";
+	document.getElementById('sec').innerHTML = "00";
+	document.getElementById('count').innerHTML = "00";
+	let typ = document.getElementById('akt').innerHTML;
+	db.run(`INSERT INTO czas (HowLong, dateActivity,typeActivity) VALUES (${ile},${now.getDate},${typ}`)
+	break;
+		}
+		else{
 		document.getElementById('hr').innerHTML = hrString;
 		document.getElementById('min').innerHTML = minString;
 		document.getElementById('sec').innerHTML = secString;
 		document.getElementById('count').innerHTML = countString;
 		setTimeout(stopWatch, 10);
+		}
+
+		
 	}
 }
 
-// let timeFormWork = db.all(`SELECT Count(HowLong) FROM czas WHERE dateActivity=${now.getDate},typeActivity=${1}`)
-// let hrtimeForWork = timeFormWork/60;
-// if(hrtimeForWork<8)
-// {
-// document.getElementById("descriptionWork").innerHTML = `Przepracowałeś ${hrtimeForWork} godzin`;
-// }
-// else{
-// 	document.getElementById("descriptionWork").innerHTML = `Przepracowałeś powyżej 8 czas na odpoczynek`;
-// }
-// let timeForLesson = db.all(`SELECT Count(HowLong) FROM czas WHERE dateActivity=${now.getDate},typeActivity=${2}`)
-// if(timeForLesson>0){
-// 	document.getElementById("descriptionLesson").innerHTML = `Uczyłeś się ${timeForLesson}`;
-// }
-// else{
-// 	document.getElementById("descriptionLesson").innerHTML = `Nie uczyłeś się dzisiaj`;
-// }
-// let timeForBook = db.all(`SELECT Count(HowLong) FROM czas WHERE dateActivity=${now.getDate},typeActivity=${3}`)
-// if(timeForBook>0){
-// 	document.getElementById("descriptionBook").innerHTML = `Czytałeś książkę ${timeForLesson} minut`;
-// }
-// else{
-// 	document.getElementById("descriptionBook").innerHTML = `Nie czytałeś dzisiaj żadnej książki`;
-// }
+let timeFormWork = db.all(`SELECT Count(HowLong) FROM czas WHERE dateActivity=${now.getDate},typeActivity=${1}`)
+let hrtimeForWork = timeFormWork/60;
+if(hrtimeForWork<8)
+{
+document.getElementById("descriptionWork").innerHTML = `Przepracowałeś ${hrtimeForWork} godzin`;
+ }
+ else{
+ 	document.getElementById("descriptionWork").innerHTML = `Przepracowałeś powyżej 8 czas na odpoczynek`;
+ }
+ let timeForLesson = db.all(`SELECT Count(HowLong) FROM czas WHERE dateActivity=${now.getDate},typeActivity=${2}`)
+ if(timeForLesson>0){
+ 	document.getElementById("descriptionLesson").innerHTML = `Uczyłeś się ${timeForLesson}`;
+ }
+ else{
+ 	document.getElementById("descriptionLesson").innerHTML = `Nie uczyłeś się dzisiaj`;
+ }
+ let timeForBook = db.all(`SELECT Count(HowLong) FROM czas WHERE dateActivity=${now.getDate},typeActivity=${3}`)
+ if(timeForBook>0){
+ 	document.getElementById("descriptionBook").innerHTML = `Czytałeś książkę ${timeForLesson} minut`;
+ }
+ else{
+ 	document.getElementById("descriptionBook").innerHTML = `Nie czytałeś dzisiaj żadnej książki`;
+ }
